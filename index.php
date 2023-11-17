25e83cd55c009a017ba62dfc61ac751c0b041aa7
ThaliumFr <theomaxmi34@gmail.com>
2023-11-17 11:21:39 +0100

    Co-authored-by: enzomtp <enzomtpYT@users.noreply.github.com> Co-authored-by: LACABANNE Alex <alex.lacabanne@ecoles-epsi.net> Co-authored-by: N4suk4 <N4suk4@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+
+
+?>
+
+<!--    
+    https://trello.com/invite/epsigroupe4/ATTI38cbeacc9a92f1c297f86d02e176aa54DC4EB502
+    https://trello.com/invite/epsigroupe4/ATTI38cbeacc9a92f1c297f86d02e176aa54DC4EB502 
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <linket" href="./style.css">
+    <title>Hotel Neptune</title>
+</head>
+<body>
+    <!-- Barre de navigation -->
+    <nav class="navbar">
+        <ul>
+            <li><a class="active navelement" href="index.php">Accueil</a></li>
+            <li><a class="navelement" href="login.php">Connexion</a></li>
+            <li><a class="navelement" href="register.php">Inscription</a></li>
+        </ul>
+    </nav>
+    
+    <!-- Container = Site entier -->
+    <div class="container">
+        
+    </div>
+</body>
+</html
