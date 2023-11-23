5095853f6c20c86cc8b4586c884482173f920d3d
enzomtpYT <enzojore@live.fr>
2023-11-23 12:50:12 +0100

    PapiSeb

--- index.php
+++ index.php
@@ -9,16 +9,21 @@ session_start(); // pas touche ou sinon au coin !
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <linket" href="./style.css">
+    <link rel="stylesheet" href="./style.css">
     <title>Hotel Neptune</title>
 </head>
 <body>
     <!-- Barre de navigation -->
     <nav class="navbar">
         <ul>
-            <li><a class="active nav-element" href="index.php">Accueil</a></li>
-            <li><a class="nav-element" href="login.php">Connexion</a></li>
-            <li><a class="nav-element" href="register.php">Inscription</a></li>
+            <li><a class="active nav-element" href="index.php">Hotel Neptune</a></li>
+            <li><a class="nav-element" href="search.php">Chambres et Suites</a></li>
+            <li><a class="nav-element" href="kitchen.php">Restauration</a></li>
+            <li><a class="nav-element" href="noboring.php">Activités</a></li>
+            <li><a class="nav-element" href="usefull.php">Services</a></li>
+            <li><a class="nav-element" href="contact.php">Contact</a></li>
+            <li><a class="nav-element button" href="login.php">Connexion</a></li>
+            <li><a class="nav-element" href="register.php"><img class="profilimage" src="./image/user.png" alt="Image de profil"></a></li>
         </ul>
     </nav>
     
@@ -30,4 +35,4 @@ session_start(); // pas touche ou sinon au coin !
         </section>
     </div>
 </body>
-</html
+</html>
