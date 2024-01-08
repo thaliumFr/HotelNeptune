48b2604ecddcad58734c26f06c6ab17947051e29
ThaliumFr <theomaxmi34@gmail.com>
2024-01-08 11:50:55 +0100

    parralax improvment

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-document.addEventListener('scroll', (e) => {
-    const parralaxPower = 0.5;
-    let scrollY = window.scrollY*parralaxPower;
-    let parallaxElements = document.querySelectorAll("*[data-parallax]");
+const parralaxPower = 0.5;
+const parallaxElements = document.querySelectorAll("*[data-parallax]");
 
+function parallax() {
+    let scrollY = window.scrollY*parralaxPower;
     parallaxElements.forEach(element => {
         element.style.backgroundPosition = "center "+scrollY+"px";
     });
+}
+
+
+document.addEventListener('scroll', (e) => {
+    parallax();
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    parallax();
 })
